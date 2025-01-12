a61ab06410167d46a390da8a89d87d0c06709f4e
dmf95 <domenicfayad@gmail.com>
2025-01-12 03:41:26 -0800

    last try...

--- utils/auth_utils.py
+++ utils/auth_utils.py
@@ -13,8 +13,21 @@ import streamlit as st
 #--------
 # PROD
 #--------
-# Access credentials and Firebase details from Streamlit secrets
-firebase_credentials = st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
+# Parse Firebase credentials from Streamlit secrets
+firebase_credentials = {
+    "type": st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]["type"],
+    "project_id": st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]["project_id"],
+    "private_key_id": st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]["private_key_id"],
+    "private_key": st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]["private_key"],
+    "client_email": st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]["client_email"],
+    "client_id": st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]["client_id"],
+    "auth_uri": st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]["auth_uri"],
+    "token_uri": st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]["client_x509_cert_url"],
+    "universe_domain": st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]["universe_domain"],
+}
+
 # Access Firebase secrets
 project_id = st.secrets["FIREBASE"]["project_id"]
 database_url = st.secrets["FIREBASE"]["database_url"]
